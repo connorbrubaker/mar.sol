1151f48c087c4d20c9520d6f4d24351390fde2c2
Connor Brubaker <brubaker@tamu.edu>
2021-12-12 02:51:30 -0600

    updated calculation of df

--- chapter2/2.1_playbill.R
+++ chapter2/2.1_playbill.R
@@ -5,7 +5,8 @@ fit <- lm(CurrentWeek ~ LastWeek, data = playbill)
 # a - confidence interval for slope
 est.slope <- as.numeric(fit$coefficients[2])
 est.slope.se <- as.numeric(summary(fit)$coefficients[2, 2])
-slope.confint <- est.slope + c(-1, 1) * qt(0.975, df = 16) * est.slope.se
+slope.confint <- est.slope + 
+    c(-1, 1) * qt(0.975, df = nrow(playbill) - 2) * est.slope.se
 print(slope.confint)
 # [1] 0.9514971 1.0126658
 
@@ -13,7 +14,7 @@ print(slope.confint)
 est.intercept <- as.numeric(fit$coefficients[1])
 est.intercept.se <- as.numeric(summary(fit)$coefficients[1, 2])
 test.statistic <- (est.intercept - 10000) / est.intercept.se
-p.value <- 2 * pt(test.statistic, df = 16, lower.tail = TRUE)
+p.value <- 2 * pt(test.statistic, df = nrow(playbill) - 2, lower.tail = TRUE)
 print(p.value)
 # [1] 0.7517807
 
